7dd90b916831602de1c8838a09a0efb672c4e872
MyselfRoshan <roshanghimire699@protonmail.com>
2023-08-19 23:57:23 +0545

    implemented password reset sucessfully

--- app/Http/Controllers/passwordReset/store.php
+++ app/Http/Controllers/passwordReset/store.php
@@ -1,16 +1,18 @@
 <?php
 
 use App\Helper\Mailer;
-use App\Http\Forms\ForgotPasswordForm;
+use App\Helper\PasswordResetToken;
+use App\Http\Forms\PasswordResetForm;
+use App\Session;
 use Database\Database;
 use PHPMailer\PHPMailer\Exception;
 
 extract($_POST);
 
-$form = new ForgotPasswordForm();
+$form = new PasswordResetForm();
 if (!$form->validate($_POST)) {
 
-    require_view('forgotPassword/index.view..php', [
+    require_view('passwordReset/index.view..php', [
         'scripts' => [
             'type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"',
             'nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"',
@@ -19,21 +21,26 @@ if (!$form->validate($_POST)) {
         'alerts' => $form->getAlerts()
     ]);
 } else {
+    $session = Session::getInstance();
+    // If reset token expired regenerate new one and save to the database
+    if ((!isset($session->reset_token) && !isset($session->reset_token_expires_at)) || strtotime($session->reset_token_expires_at) <= time()) {
+        // $token = new PasswordResetToken(32, 15);
+        $token = new PasswordResetToken();
+        $session->reset_token = $token->getToken();
+        $session->reset_token_expires_at = $token->getExpiry();
+        $query = "UPDATE public.user
+        SET reset_token_hash = :token_hash,
+            reset_token_expires_at = :token_expires_at
+        WHERE email = :email";
 
-    $token = generateRandomToken();
-    $token_hash = hash("sha256", $token);
-    $expiry = date("Y-m-d H:i:s", time() + 60 * 60);
-
-    $query = "UPDATE public.user
-    SET reset_token_hash = :token_hash,
-        reset_token_expires_at = :token_expires_at
-    WHERE email = :email";
-    $params = [
-        'token_hash' => [$token_hash, PDO::PARAM_STR],
-        'token_expires_at' => [$expiry, PDO::PARAM_STR],
-        'email' => [$email, PDO::PARAM_STR],
-    ];
-    Database::insert($query, $params);
+        $params = [
+            'token_hash' => [$token->getTokenHash(), PDO::PARAM_STR],
+            'token_expires_at' => [$token->getExpiry(), PDO::PARAM_STR],
+            'email' => [$email, PDO::PARAM_STR],
+        ];
+        Database::insert($query, $params);
+    }
+    d($session->reset_token);
 
     $mailer = Mailer::getInstance();
     // Receipts
@@ -46,12 +53,12 @@ if (!$form->validate($_POST)) {
     <p>If you did request a password reset, please click the link below to reset your password:</p>
     
     <p>
-        <a href="http://localhost/forgotPassword/{$token}" target="_blank" style="display: inline-block; padding: 10px 20px; background-color: #007bff; color: white; text-decoration: none;">Reset Password</a>
+        <a href="http://localhost/password-reset/{$session->reset_token}" target="_blank" style="display: inline-block; padding: 10px 20px; background-color: #007bff; color: white; text-decoration: none;">Reset Password</a>
     </p>
     
     <p>If the above link doesn't work, you can copy and paste the following URL into your browser:</p>
     
-    <p>http://localhost/forgotPassword/{$token}</p>
+    <p>http://localhost/password-reset/{$session->reset_token}</p>
     
     <p>This link will expire in 1 hour for security reasons.</p>
     
@@ -69,7 +76,7 @@ if (!$form->validate($_POST)) {
     
     If you did request a password reset, please copy and paste the link below into your browser's address bar to reset your password:
     
-    http://localhost/forgotPassword/{$token}
+    http://localhost/password-reset/{$session->reset_token}
     
     This link will expire in 1 hour for security reasons.
     
@@ -84,7 +91,7 @@ if (!$form->validate($_POST)) {
         $mailer->AltBody = $altBody;
         // Send the email
         $mailer->send();
-        require_view('forgotPassword/email-sent-sucessfully.view.php', [
+        require_view('passwordReset/email-sent-sucessfully.view.php', [
             'scripts' => [
                 'type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"',
                 'nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"',
