416994f171e11c6eebdc7f0b30e482b7d1e3e29b
MyselfRoshan <roshanghimire699@protonmail.com>
2023-08-21 21:15:34 +0545

    feat: added recommendation section

--- views/partials/dashboard.php
+++ views/partials/dashboard.php
@@ -1,2 +1,205 @@
-<?php
-echo "This is dashboard";
+<section class="recommendation">
+    <header>
+        <h2 class="ff-leauge-gothic fs-900 fw-700 mb-6 text-tertiary">
+            Recommendation
+        </h2>
+    </header>
+    <article class="exercise card bg-yellow">
+        <h3 class="ff-expletus fs-600 fw-500 mb-3 text-accent">
+            Morning Exercises
+            <span class="fs-300">
+                (Choose according to your needs and preferences)
+            </span>
+        </h3>
+        <div>
+            <div class="exercise__type">
+                <header class="dropdown-toggler d-flex py-1.5">
+                    <h4 class="fw-500">Yoga (15-20 minutes)</h4>
+                    <ion-icon name="chevron-down"></ion-icon>
+                </header>
+                <ul class="dropdown-items mb-1.5 ms-5">
+                    <li>
+                        Practice sun salutations (Surya Namaskar) to awaken your
+                        body and mind.
+                    </li>
+                    <li>
+                        Incorporate poses like Downward Dog, Cobra, Child's Pose,
+                        and Tree Pose.
+                    </li>
+                    <li>Focus on deep breathing and mindfulness.</li>
+                </ul>
+                <button class="btn btn-s mb-2 ms-5 mb-2">Start Now</button>
+            </div>
+            <div class="exercise__type">
+                <header class="dropdown-toggler d-flex py-1.5">
+                    <h4 class="fw-500">Cardio Workout (20-30 minutes)</h4>
+                    <ion-icon name="chevron-down"></ion-icon>
+                </header>
+                <ul class="dropdown-items mb-1.5 ms-5">
+                    <li>
+                        Go for a brisk walk, jog, or run in your neighborhood or a
+                        nearby park.
+                    </li>
+                    <li>
+                        If you have a stationary bike or jump rope, incorporate
+                        those into your routine.
+                    </li>
+                </ul>
+                <button class="btn btn-s mb-2 ms-5">Start Now</button>
+            </div>
+            <div class="exercise__type">
+                <header id="dropdown-toggler" class="dropdown-toggler d-flex py-1.5">
+                    <h4 class="fw-500">Dance Workout (15-20 minutes)</h4>
+                    <ion-icon name="chevron-down"></ion-icon>
+                </header>
+                <ul class="dropdown-items mb-1.5 ms-5">
+                    <li>
+                        Put on your favorite music and engage in a dance workout
+                        to get your heart rate up and have fun.
+                    </li>
+                </ul>
+                <button class="btn btn-s mb-2 ms-5 mb-2">Start Now</button>
+            </div>
+        </div>
+    </article>
+
+    <article class="hydration card bg-ash">
+        <h3 class="ff-expletus fs-600 fw-500 mb-3 text-accent">
+            Stay Hydrated
+            <span class="fs-300">
+                (Keep yourself refreshed throughout the day)
+            </span>
+        </h3>
+        <div class="hydration__info">
+            <ul class="ms-5">
+                <li>
+                    Drink water to maintain good health and stay energized.
+                </li>
+                <li>
+                    Aim for at least 8 glasses (about 2 liters) of water daily.
+                </li>
+                <li>
+                    Carry a reusable water bottle for easy hydration on-the-go.
+                </li>
+            </ul>
+        </div>
+        <button class="btn btn-s ms-5 mt-2">Stay Hydrated</button>
+    </article>
+
+    <article class="sleep card bg-dark-900">
+        <h3 class="ff-expletus fs-600 fw-500 mb-3 text-secondary">
+            Quality Sleep
+            <span class="fs-300">
+                (Refresh your body and mind with good sleep)
+            </span>
+        </h3>
+        <div class="sleep__info">
+            <header class="dropdown-toggler d-flex text-yellow py-1.5">
+                <h4 class="fw-500">Prioritize Sound Sleep (7-9 hours)</h4>
+                <ion-icon name="chevron-down"></ion-icon>
+            </header>
+            <ul class="dropdown-items mb-1.5 ms-5">
+                <li>Ensure quality sleep for overall well-being.</li>
+                <li>Aim for 7-9 hours of sleep per night.</li>
+                <li>Create a comfortable sleep environment.</li>
+            </ul>
+        </div>
+
+        <div class="sleep__info">
+            <header class="dropdown-toggler d-flex text-yellow py-1.5">
+                <h4 class="fw-500">Establish a Bedtime Routine</h4>
+                <ion-icon name="chevron-down"></ion-icon>
+            </header>
+            <ul class="dropdown-items mb-1.5 ms-5">
+                <li>Develop consistent sleep and wake times.</li>
+                <li>Wind down with calming activities before bed.</li>
+                <li>Limit screen time and exposure to bright lights.</li>
+            </ul>
+        </div>
+
+        <div class="sleep__info">
+            <header class="dropdown-toggler d-flex text-yellow py-1.5">
+                <h4 class="fw-500">Healthy Sleep Habits</h4>
+                <ion-icon name="chevron-down"></ion-icon>
+            </header>
+            <ul class="dropdown-items mb-1.5 ms-5">
+                <li>Avoid heavy meals and caffeine close to bedtime.</li>
+                <li>Engage in relaxation techniques like deep breathing.</li>
+                <li>Invest in a supportive mattress and pillows.</li>
+            </ul>
+        </div>
+        <button class="btn btn-s mt-2 ms-5">Improve Sleep</button>
+    </article>
+
+    <article class="food-intake card bg-primary-800">
+        <h3 class="ff-expletus fs-600 fw-500 mb-3 text-dark">
+            Balanced Nutrition
+            <span class="fs-300">
+                (Nourish your body with healthy food choices)
+            </span>
+        </h3>
+
+        <div class="food-intake__info">
+            <header class="dropdown-toggler d-flex text-accent py-1.5">
+                <h4 class="fw-500">Choose Nutrient-Rich Foods</h4>
+                <ion-icon name="chevron-down"></ion-icon>
+            </header>
+            <ul class="dropdown-items mb-1.5 ms-5">
+                <li>
+                    Prioritize whole grains, lean proteins, and fresh
+                    vegetables.
+                </li>
+                <li>
+                    Incorporate a variety of colors and textures into your
+                    meals.
+                </li>
+                <li>
+                    Include healthy fats and limit saturated and trans fats.
+                </li>
+            </ul>
+        </div>
+
+        <div class="food-intake__info">
+            <header class="dropdown-toggler d-flex text-accent py-1.5">
+                <h4 class="fw-500">Portion Control</h4>
+                <ion-icon name="chevron-down"></ion-icon>
+            </header>
+            <ul class="dropdown-items mb-1.5 ms-5">
+                <li>Be mindful of portion sizes to avoid overeating.</li>
+                <li>Listen to your body's hunger and fullness cues.</li>
+                <li>Avoid eating in front of screens or distractions.</li>
+            </ul>
+        </div>
+
+        <div class="food-intake__info">
+            <header class="dropdown-toggler d-flex text-accent py-1.5">
+                <h4 class="fw-500">Stay Hydrated</h4>
+                <ion-icon name="chevron-down"></ion-icon>
+            </header>
+            <ul class="dropdown-items mb-1.5 ms-5">
+                <li>Drink plenty of water throughout the day.</li>
+                <li>
+                    Limit sugary drinks and opt for water, herbal tea, or
+                    infused water.
+                </li>
+            </ul>
+        </div>
+
+        <div class="food-intake__info">
+            <header class="dropdown-toggler d-flex text-accent py-1.5">
+                <h4 class="fw-500">Time Period and Frequency</h4>
+                <ion-icon name="chevron-down"></ion-icon>
+            </header>
+            <ul class="dropdown-items mb-1.5 ms-5">
+                <li>Have balanced meals every 3-4 hours.</li>
+                <li>Include a variety of nutrients in each meal.</li>
+                <li>
+                    Snack on healthy options between meals to maintain energy
+                    levels.
+                </li>
+            </ul>
+        </div>
+
+        <button class="btn btn-s mt-2 ms-5">Eat Well</button>
+    </article>
+</section>
