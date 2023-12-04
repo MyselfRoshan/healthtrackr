15bab80f2d4381cc3ecec24ab57f045b35972e0a
MyselfRoshan <roshanghimire699@protonmail.com>
2023-12-05 00:34:39 +0545

    refactor: 404

--- views/404.php
+++ views/404.php
@@ -4,15 +4,20 @@
 <head>
   <title>Slide Navbar</title>
   <link rel="stylesheet" type="text/css" href="/resources/css/main.css" />
+  <script defer type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script defer nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </head>
 
 <body>
   <section class="page-not-found">
     <!-- <img class="page-not-found-img" src="/resources/images/404.jpg" alt="Oops!!! Something Went wrong. Error 404 Page Not Found" /> -->
     <?php require_svg("404.svg") ?>
-    <!-- <a href="/">&leftarrow; Back to Home</a> -->
     <a href="/">
-      < Back to Home</a>
+      <div class="svg">
+        <?php require_svg("back-arrow.svg") ?>
+      </div>
+      Back to Home
+    </a>
   </section>
 </body>
 
